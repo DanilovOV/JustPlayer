5c475587307a1c8367c3dcc26ff2d26487abb96d
Oleg <oleg-danilov8@ya.ru>
2022-11-22 23:22:43 +0300

    upd: Audioplayer class

--- src/scripts/Audioplayer.js
+++ src/scripts/Audioplayer.js
@@ -1,5 +1,8 @@
 class Audioplayer {
-    constructor() {
 
+    constructor(playerElem) {
+        if (!playerElem) throw new Error('Constructor require system player HTML elem')
+        this.playerElem = playerElem
+        
     }
 }
